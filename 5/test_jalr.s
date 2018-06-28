635ea079e601074bd9a8de8dfc82daff4387a3b7
MauroFab <maurotoscano2@gmail.com>
2018-06-28 13:25:31 -0300

    Correciones varias

--- 5/test_jalr.s
+++ 5/test_jalr.s
@@ -1,7 +1,7 @@
 	li	$t0, 111
 	li	$t1, 222
 	li	$t2, 24
-	jalr	$t2, $t3
+	jalr	$t3, $t2
 
 	# las siguientes 2 instrucciones no deberian ejecutarse si el salto se realiza
 	addi	$t0, $t0, 333
@@ -9,4 +9,3 @@
 
 test:
        li $t2, 555
-
